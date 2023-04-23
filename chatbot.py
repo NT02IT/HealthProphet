5f0e32295727368922d2186b5ecd221466802a61
AgonDrake <agondrake@gmail.com>
2023-04-23 15:24:18 +0700

    Add select question

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,7 @@ from chatterbot import ChatBot
 
 app = Flask(__name__)
 
+#Khởi tạo chatbot
 chatbot = ChatBot(
     'HealthProphet',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
@@ -15,20 +16,22 @@ chatbot = ChatBot(
     ]
 )
 
+#Trả về trang response.html
 @app.route("/response")
 def response():
     return render_template("response.html")
 
+#Trả về trang home - index.html
 @app.route("/home")
 def home():
     return render_template("index.html")
 
+#Nhận và reply tin nhắn
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
     return str(chatbot.get_response(userText))
 
-
+#Run flask
 if __name__ == "__main__":
     app.run(debug=True, port=5500)
-
